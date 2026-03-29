fa3fe65dae8fdbd8b07d4e6265f381b6d17910c3
KonradMast <s28135@pjwstk.edu.pl>
2026-03-29 22:18:55 +0200

    finalna wersja aplikacji - scenariusz demonstracyjny i raporty

--- APBD-Cw1-s28135/Program.cs
+++ APBD-Cw1-s28135/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using APBD_Cw1_s28135.Models;
+using APBD_Cw1_s28135.Repositories;
+using APBD_Cw1_s28135.Services;
+using APBD_Cw1_s28135.UI;
 
-Console.WriteLine("Hello, World!");
+// inicjalizacja komponentow
+var repository = new StoreRepository();
+var rentalService = new RentalService(repository);
+var reportService = new ReportService(repository);
+
+// dodanie danych (uzytkownicy i sprzet
+repository.AddUser(new Student(1, "Jan", "Kowalski"));
+repository.AddUser(new Employee(2, "Anna", "Nowak"));
+
+repository.AddEquipment(new Laptop(101, "Dell XPS", "Intel i7", 16));
+repository.AddEquipment(new Laptop(102, "MacBook Pro", "M2", 32));
+repository.AddEquipment(new Projector(201, "Epson EB", "4K"));
+
+// scenariusz wypozyczen
+Console.WriteLine("--- DEMONSTRACJA DZIALANIA ---");
+
+// poprawne wypozyczenie
+Console.WriteLine(rentalService.RentEquipment(1, 101)); 
+
+// proba wypozyczenia tego samego sprzetu
+Console.WriteLine(rentalService.RentEquipment(2, 101)); 
+
+// przekroczenie limitu(max 2) przez studenta 
+rentalService.RentEquipment(1, 102);
+Console.WriteLine("Proba przekroczenia limitu przez studenta:");
+Console.WriteLine(rentalService.RentEquipment(1, 201)); 
+
+// zwrot sprzetu
+Console.WriteLine("\n--- ZWROTY ---");
+Console.WriteLine(rentalService.ReturnEquipment(101));
+
+// raport
+reportService.PrintStatusReport();
---
+++ APBD-Cw1-s28135/UI/ReportService.cs
@@ -0,0 +1,24 @@
+﻿using APBD_Cw1_s28135.Repositories;
+
+namespace APBD_Cw1_s28135.UI;
+
+public class ReportService(StoreRepository repository)
+{
+    public void PrintStatusReport()
+    {
+        Console.WriteLine("\n--- RAPORT KONCOWY STANU SYSTEMU ---");
+        
+        Console.WriteLine("\nUzytkownicy:");
+        repository.Users.ForEach(Console.WriteLine);
+
+        Console.WriteLine("\nSprzet:");
+        repository.EquipmentList.ForEach(Console.WriteLine);
+
+        Console.WriteLine("\nAktywne Wypozyczenia:");
+        var active = repository.Rentals.Where(r => r.ReturnDate == null).ToList();
+        if (active.Count == 0) Console.WriteLine("Brak.");
+        else active.ForEach(Console.WriteLine);
+        
+        Console.WriteLine("------------------------------------\n");
+    }
+}
